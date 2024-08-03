b67bc612ef62c009e960b9b334a76d37e39f2a1b
Peter Osas <osaspeter6@gmail.com>
2024-08-04 00:48:48 +0100

    feat: added a riverpod provider

--- lib/simple_provider/simple_provider.dart
+++ lib/simple_provider/simple_provider.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_practice/simple_provider/provider.dart';
 
-class SimpleProver extends ConsumerWidget {
-  const SimpleProver({super.key});
+class SimpleProvider extends ConsumerWidget {
+  const SimpleProvider({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Provider"),
+      ),
+      body: Center(
+        child: Text(date.toIso8601String()),
       ),
     );
   }
