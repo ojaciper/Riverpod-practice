a4dcdbda16804a5b182fe0a766b32ebf18e5084b
Peter Osas <osaspeter6@gmail.com>
2024-08-04 09:36:34 +0100

    feat: added futur provider

---
+++ lib/future_provider/future_provider.dart
@@ -0,0 +1,35 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+enum City {
+  stockholm,
+  paris,
+  tokyo,
+}
+
+const unknowWeatherEmoji = "🤷‍♂️";
+
+typedef WeatherEmoji = String;
+
+Future<WeatherEmoji> getWeather(City city) {
+  return Future.delayed(
+    const Duration(seconds: 1),
+    () =>
+        {
+          City.stockholm: "❄️",
+          City.paris: "🌧️",
+          City.tokyo: "🌤️",
+        }[city] ??
+        "🔥",
+  );
+}
+
+final currentCityProvider = StateProvider<City?>((ref) => null);
+
+final weatherProvider = FutureProvider<WeatherEmoji>((ref) {
+  final city = ref.watch(currentCityProvider);
+  if (city != null) {
+    return getWeather(city);
+  } else {
+    return unknowWeatherEmoji;
+  }
+});
